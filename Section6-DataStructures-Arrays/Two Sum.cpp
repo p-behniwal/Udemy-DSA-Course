017708ecb00ba9e4d902dafd8d3cd3d23c9f93ea
Prabhgun Behniwal <prabhgun@Prabhguns-MacBook-Pro.local>
2020-11-22 22:27:52 -0500

    fixed error

--- Section6-DataStructures-Arrays/Two Sum.cpp
+++ Section6-DataStructures-Arrays/Two Sum.cpp
@@ -2,15 +2,18 @@ using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        Map<int,int> seen = new HashMap<>();
+        unordered_map<int,int> seen;
+        vector<int> answer;
         for(int i = 0; i < nums.size(); ++i){
             int need = target - nums[i];
-            if (seen.containsKey(need)){
-                return new int[] {seen.get(need), i};
-
+            if (seen.find(need) != seen.end()){
+                answer.push_back(seen[need]);
+                answer.push_back(i);
+                return answer;
             }
-            seen.put(nums[i], i);
+            seen.insert(make_pair(nums[i], i));
         }
+        return answer;
         
     }
 };
