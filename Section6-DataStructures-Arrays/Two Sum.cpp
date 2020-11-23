c2283e9149bf4487452395a05c4dd8252a92d522
Prabhgun Behniwal <prabhgun@Prabhguns-MacBook-Pro.local>
2020-11-22 20:19:19 -0500

    two sum solution

--- Section6-DataStructures-Arrays/Two Sum.cpp
+++ Section6-DataStructures-Arrays/Two Sum.cpp
@@ -1,6 +1,16 @@
+using namespace std;
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        Map<int,int> seen = new HashMap<>();
+        for(int i = 0; i < nums.size(); ++i){
+            int need = target - nums[i];
+            if (seen.containsKey(need)){
+                return new int[] {seen.get(need), i};
+
+            }
+            seen.put(nums[i], i);
+        }
         
     }
 };
