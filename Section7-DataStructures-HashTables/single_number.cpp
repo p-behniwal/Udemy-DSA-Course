46680ef8fe027d145a718998056f1f10c775477a
Prabhgun Behniwal <prabhgun@Prabhguns-MacBook-Pro.local>
2021-01-01 20:26:03 -0500

    trivial hash table search

---
+++ Section7-DataStructures-HashTables/single_number.cpp
@@ -0,0 +1,16 @@
+/*
+Simple hash search for occurence
+*/
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int> track;
+        for (int i = 0; i < nums.size(); i++) {
+            track[nums[i]]++;
+        }
+        for (auto it: track) {
+            if(it.second == 1) return it.first;
+        }
+        return nums[0];
+    }
+};
